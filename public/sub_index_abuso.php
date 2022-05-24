83ee6323512f9b2b47fcab857f451520a220bf6a
Juan Diego Jaramillo <33137383+judijasa@users.noreply.github.com>
2022-05-24 15:40:06 -0500

    Update sub_index_abuso.php

--- public/sub_index_abuso.php
+++ public/sub_index_abuso.php
@@ -13,7 +13,7 @@
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
     
     <link rel="stylesheet" type="text/css"
-        href="./styles/main.css">
+        href="./main.css">
         
 </head>
 
