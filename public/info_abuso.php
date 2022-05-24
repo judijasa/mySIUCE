5ff945a3447e4011bda434351bb3fb0bbbc83ef1
Juan Diego Jaramillo <33137383+judijasa@users.noreply.github.com>
2022-05-24 15:35:39 -0500

    Update info_abuso.php

--- public/info_abuso.php
+++ public/info_abuso.php
@@ -12,7 +12,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
 
-    <link rel="stylesheet" type="text/css" href="./styles/main.css">
+    <link rel="stylesheet" type="text/css" href="./main.css">
 
 </head>
 
