9312da4271205832d8b5bd1ab1f5b56e207d74c7
Juan Diego Jaramillo <33137383+judijasa@users.noreply.github.com>
2022-06-07 14:56:10 -0500

    Update abuso_repo_handling.php

--- public/abuso_repo_handling.php
+++ public/abuso_repo_handling.php
@@ -6,7 +6,7 @@
         <!-- https://codepen.io/fainder/pen/AydHJ -->
         <meta charset="utf-8">
         <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" type="text/css" href="./styles/main.css">
+        <link rel="stylesheet" type="text/css" href="./main.css">
     </head>
 
     <body>
