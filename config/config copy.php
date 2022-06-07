3d2f50a36be275642ff0797fb9ba81288aec7164
Juan Diego Jaramillo <33137383+judijasa@users.noreply.github.com>
2022-06-07 14:52:42 -0500

    Add files via upload

---
+++ config/config copy.php
@@ -0,0 +1,9 @@
+<?php
+    //  Database keys are kept in this private file while their alias is used in the public php
+        $servername = "XXXXXXXX";
+        $username = "XXXXXXXX";
+        $password = "XXXXXXXXXX";
+        $dbname = "XXXXXXXXXX";
+        //$home = "XXXXXXXXX"
+?>
+
